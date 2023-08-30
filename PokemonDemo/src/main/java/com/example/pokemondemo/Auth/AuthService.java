b2c20427d6713e4a9b70b75450fa513b577e9eb8
catadecasm <Juliana-Catalina.De@endava.com>
2023-08-30 18:35:23 -0500

    User package implemented - Role and User

--- PokemonDemo/src/main/java/com/example/pokemondemo/Auth/AuthService.java
+++ PokemonDemo/src/main/java/com/example/pokemondemo/Auth/AuthService.java
@@ -1,5 +1,11 @@
 package com.example.pokemondemo.Auth;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
 public class AuthService {
     public AuthResponse login(LoginRequest request){
         return null;
---
+++ PokemonDemo/src/main/java/com/example/pokemondemo/User/Role.java
@@ -0,0 +1,8 @@
+package com.example.pokemondemo.User;
+
+public enum Role {
+    ADMIN,
+    TRAINER,
+    DOCTOR,
+    PROFESSOR_OAK,
+}
