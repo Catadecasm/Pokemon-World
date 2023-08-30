72fde09cb2c49edefb5986fc84e06d4226a8507f
catadecasm <Juliana-Catalina.De@endava.com>
2023-08-30 18:02:44 -0500

    PostMappingUpdated in AuthController

---
+++ PokemonDemo/src/main/java/com/example/pokemondemo/Auth/AuthService.java
@@ -0,0 +1,11 @@
+package com.example.pokemondemo.Auth;
+
+public class AuthService {
+    public AuthResponse login(LoginRequest request){
+        return null;
+    }
+    public AuthResponse register(RegisterRequest request){
+        return  null;
+    }
+}
+
