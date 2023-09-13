bc14cfbb9b8bb2af8f6d95d48f4cb43da0f4fca1
catadecasm <Juliana-Catalina.De@endava.com>
2023-09-13 14:04:39 -0500

    Save and retrieve pokemon un save pokemon Test

--- PokemonDemo/src/test/java/com/example/pokemondemo/PokemonDemoApplicationTests.java
+++ PokemonDemo/src/test/java/com/example/pokemondemo/PokemonDemoApplicationTests.java
@@ -2,18 +2,29 @@ package com.example.pokemondemo;
 
 import com.example.pokemondemo.entity.Pokemon;
 import com.example.pokemondemo.entity.User;
+import com.example.pokemondemo.model.dto.ClassicResponseDTO;
 import com.example.pokemondemo.model.dto.PokemonDTO;
+import com.example.pokemondemo.model.dto.TrainerPokedexDTO;
 import com.example.pokemondemo.model.dto.pokeapi.SingleEsPokemonDTO;
 import com.example.pokemondemo.repository.PokemonRepository;
 import com.example.pokemondemo.repository.UserRepository;
 import com.example.pokemondemo.service.PokemonService;
 import com.example.pokemondemo.exception.NotFoundException;
+import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.pokemondemo.service.pokeapi.PokedexPokemonSpecServiceImplService;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class PokemonDemoApplicationTests {
@@ -31,14 +42,15 @@ class PokemonDemoApplicationTests {
 
 
     }
+
     @Test
-    public void Save_Pokemon(){
+    public void Save_Pokemon() {
         this.pokedexPokemonSpecServiceImpl = new PokedexPokemonSpecServiceImplService();
         PokemonDTO pokemonDTO = PokemonDTO.builder()
                 .name("my little fire")
                 .specie("charmander")
                 .build();
-        User user =  userRepository.findByEmailIgnoreCase("willy@endava.com").get();
+        User user = userRepository.findByEmailIgnoreCase("willy@endava.com").get();
         Pokemon pokemonSave = Pokemon.builder()
                 .name(pokemonDTO.getName())
                 .specie(pokemonDTO.getName())
@@ -50,5 +62,37 @@ class PokemonDemoApplicationTests {
         Assertions.assertThat(pokemonSave.getId()).isGreaterThan(0);
     }
 
+    @Test
+    @Transactional
+    public void SaveAndRetrieve_Pokemon() {
+        // Crear un usuario
+        User user = userRepository.findByEmailIgnoreCase("willy@endava.com").get();
 
+        // Crear un Pokemon DTO
+        PokemonDTO pokemonDTO = PokemonDTO.builder()
+                .name("my little fire")
+                .specie("charmander")
+                .build();
+
+        // Guardar el Pokemon en la base de datos
+        Pokemon pokemonToSave = Pokemon.builder()
+                .name(pokemonDTO.getName())
+                .specie(pokemonDTO.getSpecie())
+                .user(user)
+                .image("dasd")
+                .build();
+        pokemonToSave = pokemonRepository.save(pokemonToSave);
+
+        // Recuperar el Pokemon de la base de datos
+        Pokemon retrievedPokemon = pokemonRepository.findById(pokemonToSave.getId()).orElse(null);
+
+        // Verificar que el Pokemon se ha recuperado correctamente
+        assertThat(retrievedPokemon).isNotNull();
+        assertThat(retrievedPokemon.getName()).isEqualTo(pokemonDTO.getName());
+        assertThat(retrievedPokemon.getSpecie()).isEqualTo(pokemonDTO.getSpecie());
+        assertThat(retrievedPokemon.getImage()).isEqualTo("dasd");
+        assertThat(retrievedPokemon.getUser()).isEqualTo(user);
+    }
 }
+
+
