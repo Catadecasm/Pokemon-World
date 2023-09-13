2453d413259e408b788b117e93dc08b6f77de158
SantiagoGV29 <gu_santiago@javeriana.edu.co>
2023-09-13 00:41:56 -0500

    Add testing to the s02 feature

--- PokemonDemo/src/main/java/com/example/pokemondemo/service/UserService.java
+++ PokemonDemo/src/main/java/com/example/pokemondemo/service/UserService.java
@@ -115,11 +115,11 @@ public class UserService {
     }
 
     public LogInResponse logInUser(LogInRequest logInRequest) {
-        /*
+        String email = logInRequest.getEmail();
         User user1 = userRepository.findByEmailIgnoreCase(email).get();
         if(user1.isLogged()){
             throw new NotFoundException("The user is already logged");
-        }*/
+        }
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken
                         (logInRequest.getEmail()
@@ -197,65 +197,4 @@ public class UserService {
         throw new NotFoundException("You don't have not provided adequate credentials to access this resource");
     }
 
-    // CRUD operations
-    public List<UserDTO> findAll() {
-        final List<User> users = userRepository.findAll(Sort.by("id"));
-        return users.stream().map(user -> mapToDTO(user, new UserDTO())).toList();
-    }
-
-    public UserDTO get(final Integer id) {
-        return userRepository.findById(id)
-                .map(user -> mapToDTO(user, new UserDTO()))
-                .orElseThrow(NotFoundException::new);
-    }
-
-    public Integer create(final UserDTO userDTO) {
-        final User user = new User();
-        mapToEntity(userDTO, user);
-        return userRepository.save(user).getId();
-    }
-
-    public void update(final Integer id, final UserDTO userDTO) {
-        final User user = userRepository.findById(id).orElseThrow(NotFoundException::new);
-        mapToEntity(userDTO, user);
-        userRepository.save(user);
-    }
-
-    public void delete(final Integer id) {
-        final User user = userRepository.findById(id).orElseThrow(NotFoundException::new);
-        // remove many-to-many relations at owning side
-        fightRepository.findAllByUserFightUsers(user).forEach(fight -> fight.getUserFightUsers().remove(user));
-        userRepository.delete(user);
-
-    }
-
-    private UserDTO mapToDTO(final User user, final UserDTO userDTO) {
-
-        userDTO.setId(user.getId());
-        userDTO.setEmail(user.getEmail());
-        userDTO.setPassword(user.getPassword());
-        userDTO.setUsername(user.getRealUsername());
-        userDTO.setRole(user.getRole() == null ? null : String.valueOf(user.getRole()));
-        userDTO.setLeagueid(user.getLeagueid() == null ? null : user.getLeagueid().getId());
-        return userDTO;
-    }
-
-    private User mapToEntity(final UserDTO userDTO, final User user) {
-        user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
-        user.setUsername(userDTO.getUsername());
-        final Role role = userDTO.getRole() == null ? null : DefineRole(userDTO.getRole());
-        user.setRole(role);
-        final League leagueid = userDTO.getLeagueid() == null ? null : leagueRepository.findById(userDTO.getLeagueid()).orElseThrow(() -> new NotFoundException("leagueid not found"));
-        user.setLeagueid(leagueid);
-        return user;
-    }
-
-    public boolean emailExists(final String email) {
-        return userRepository.existsByEmailIgnoreCase(email);
-    }
-
-    public boolean usernameExists(final String username) {
-        return userRepository.existsByUsernameIgnoreCase(username);
-    }
 }
---
+++ PokemonDemo/src/test/java/com/example/pokemondemo/authentication/LoginTest.java
@@ -0,0 +1,57 @@
+package com.example.pokemondemo.authentication;
+
+import com.example.pokemondemo.entity.User;
+import com.example.pokemondemo.exception.NotFoundException;
+import com.example.pokemondemo.model.authorization.LogInRequest;
+import com.example.pokemondemo.model.authorization.LogInResponse;
+import com.example.pokemondemo.repository.UserRepository;
+import com.example.pokemondemo.service.UserService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class LoginTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    void Validate_Multiple_Session() {
+        LogInRequest logInRequest = LogInRequest.builder()
+                .email("hola@endava.com")
+                .password("123456")
+                .build();
+        LogInResponse logInResponse =  userService.logInUser(logInRequest);
+        assertThat(logInResponse).isNotNull();
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> userService.logInUser(logInRequest));
+        assertThat(exception.getMessage()).isEqualTo("The user is already logged");
+    }
+    @Test
+    void Validate_User_Not_Found() {
+        LogInRequest logInRequest = LogInRequest.builder()
+                .email("hola@endava.com")
+                .password("12346")
+                .build();
+
+        Exception exception = assertThrows(Exception.class, () -> userService.logInUser(logInRequest));
+        assertThat(exception.getMessage()).isEqualTo("Bad credentials");
+    }
+
+    @AfterAll
+    public static void cleanUp(@Autowired UserRepository userRepository) {
+        List<User> users = userRepository.findAll();
+        for (User user : users) {
+            user.setLogged(false);
+            userRepository.save(user);
+        }
+    }
+
+}
