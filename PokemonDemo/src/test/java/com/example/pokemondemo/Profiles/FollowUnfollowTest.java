f8b869fb2de0252d997e9cd927bd5e10c7cb9a43
SantiagoGV29 <gu_santiago@javeriana.edu.co>
2023-09-14 20:52:18 -0500

    Add follow and unfollow test

--- PokemonDemo/src/test/java/com/example/pokemondemo/Profiles/FollowUnfollowTest.java
+++ PokemonDemo/src/test/java/com/example/pokemondemo/Profiles/FollowUnfollowTest.java
@@ -1,6 +1,7 @@
 package com.example.pokemondemo.Profiles;
 
 import com.example.pokemondemo.exception.NotFoundException;
+import com.example.pokemondemo.model.dto.ClassicResponseDTO;
 import com.example.pokemondemo.service.FollowService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ class FollowUnfollowTest {
     @Autowired
     FollowService followService;
 
-    
+
     @Test
     public void followExceptions() {
         NotFoundException exception = assertThrows(NotFoundException.class, () -> followService.follow("willy@endava.com", "notrealUser"));
@@ -27,5 +28,25 @@ class FollowUnfollowTest {
         exception = assertThrows(NotFoundException.class, () -> followService.follow("andy@endava.com", "willy"));
         assertThat(exception.getMessage()).isEqualTo("You are already following this trainer");
 
+    }
+    @Test
+    public void unfollowExceptions(){
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> followService.unfollow("willy@endava.com","notrealUser"));
+        assertThat(exception.getMessage()).isEqualTo("The trainer does not exist");
+
+        exception = assertThrows(NotFoundException.class, () -> followService.unfollow("willy@endava.com","satu"));
+        assertThat(exception.getMessage()).isEqualTo("The trainer is not followed by you");
+    }
+
+    @Test
+    public void followShouldReturnIdFollow(){
+        ClassicResponseDTO response = followService.follow("willy@endava.com", "andy");
+        assertThat(response.getResponseCode()).isEqualTo("OK");
+        assertThat(response.getResponseMessage()).isEqualTo("willy is now following andy");
+
+        // Unfollow
+        response = followService.unfollow("willy@endava.com","andy");
+        assertThat(response.getResponseCode()).isEqualTo("OK");
+        assertThat(response.getResponseMessage()).isEqualTo("willy is not following andy anymore");
     }
 }
